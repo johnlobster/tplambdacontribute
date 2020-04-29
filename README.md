a88097c74445434d9d09e4e6d626918cd9e77440
John Webster <JohnLobster@comcast.net>
2020-04-29 12:48:56 -0700

    Initial setup, prints request if valid

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# tplambdacontribute
+ # tplambdacontribute
+
+lambda server to pass contributions from Toilet Paper Crisis website to email
+
+**John Webster**
+
+Github : https://github.com/johnlobster
+
+LinkedIn : https://www.linkedin.com/in/johnwwebster/
+
+Portfolio : https://johnlobster.github.io/portfolio/
+
+#### Required environment variables
+
+`TP_HOST_NAME`
+The host name of tp crisis website, eg www.ToiletPaperCrisis.com, or netlify name etc. during development
+
+`TP_EMAIL_ADDRESS`
+Email address to send contributed information to 
---
+++ server.js
@@ -0,0 +1,73 @@
+// server for tpCrisis web site, responds to POST
+// cross-server, only accepts requests from tpcrisis website
+
+require("dotenv").config(); // add variables in .env file to process.env
+const path = require("path");
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+// To do get environment variables and check for validity. Use them for all host names, etc.
+// set up express
+const app = express();                                                                            // Define middleware here                                                                         app.use(express.urlencoded({ extended: true }));                                                  app.use(express.json());
+
+// middleware
+app.use(express.json());
+
+/* To do security
+   check https
+   check originating website
+   check cors
+*/
+app.post("*", function (req, res) { 
+  // (name="", subject="", message="")
+  console.log("Message received");
+
+  let mailString= "";
+  // check for valid json in request body
+  if ((!req.body) || ((!req.body.name) && (!req.body.subject) && (!req.body.message)) ) {
+    res.json({ contributeReturn: "No content" }); 
+  }
+  // fill in missing fields if necessary
+  let userName = "";
+  if (! req.body.name ) {
+    userName="";
+  } else {
+    userName=req.body.name;
+  }
+  let userSubject = "";
+  if (!req.body.subject) {
+    userSubject = "";
+  } else {
+    userSubject = req.body.subject;
+  }
+  let userMessage = "";
+  if (!req.body.message) {
+    userMessage = "";
+  } else {
+    userMessage = req.body.message
+  }
+
+  // To do check for invalid characters and escape them
+  // To do Check that maximum string length not exceeded
+  
+  mailString = `
+  Name: ${userName}
+  Subject: ${userSubject}
+
+  ${userMessage}
+
+  Send date: 
+
+  `
+  // To do send to mail, check return result, wait for timeout
+  // return "Contribution failed"
+
+  console.log(mailString);
+  res.json({ contributeReturn: "Success" }); 
+
+});
+
+app.listen(PORT, () => { 
+  console.log(`🌎 ==> tp lambda contributions server listening on port ${PORT}`); 
+});
